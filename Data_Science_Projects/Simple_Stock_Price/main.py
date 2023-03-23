521c5d76f6a630ea6820024b7f8fa48832ad0ec7
Kiril Ivanov <kiril_ivanov_04@abv.bg>
2023-03-23 20:50:57 +0200

    First Project Done

--- Data_Science_Projects/Simple_Stock_Price/main.py
+++ Data_Science_Projects/Simple_Stock_Price/main.py
@@ -1,14 +1,22 @@
 import yfinance as yf
 import streamlit as st
 
+st.write(""" 
+# Simple Stock Price App 
 
-st.write(" # Simple Stock Price App")
+Shown are the stock closing price and volume of Google!
+""")
 
 tickerSymbol = 'GOOGL'
+tickerData = yf.Ticker(tickerSymbol)
+tickerDf = tickerData.history(period='id', start='2010-5-31', end='2020-5-31')
 
-tickeData = yf.Ticker(tickerSymbol)
-
-tickerDf = tickeData.history(period='id', start='2010-5-31', end='2020-5-31')
-
+st.write(""" 
+## Closing Price
+""")
 st.line_chart(tickerDf.Close)
+
+st.write(""" 
+## Volume Price
+""")
 st.line_chart(tickerDf.Volume)
